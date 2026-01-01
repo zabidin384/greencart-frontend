e9041e1fcb1782a3bf7a2af0c2ae54e13e382b9b
Zainal Abidin <zabidin384@gmail.com>
2026-01-01 23:34:45 +0700

    Change footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,8 @@ const Footer = () => {
 				<div>
 					<img className="w-34 md:w-32" src={assets.logo} alt="logo" />
 					<p className="max-w-[410px] mt-6">
-						We deliver fresh groceries and snacks straight to your door. Trusted by thousands, we aim to make your shopping experience simple and affordable
+						We deliver fresh groceries and snacks straight to your door. Trusted by thousands, we aim to make your shopping experience
+						simple and affordable
 					</p>
 				</div>
 				<div className="flex flex-wrap justify-between w-full md:w-[45%] gap-5">
@@ -27,7 +28,7 @@ const Footer = () => {
 					))}
 				</div>
 			</div>
-			<p className="py-4 text-center text-sm md:text-base text-gray-500/80">Copyright {new Date().getFullYear()} © Zain Dev All Right Reserved.</p>
+			<p className="py-4 text-center text-sm md:text-base text-gray-500/80">Copyright 2025 © Zainal All Right Reserved.</p>
 		</div>
 	);
 };
